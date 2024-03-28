deb4cdc74a0d75e02a2d1c62e2c41230518c9a37
davidekete <eketedavid@gmail.com>
2024-03-28 16:50:18 +0100

    refactor: remove comments in audioProcessor.js

--- src/audioProcessor.js
+++ src/audioProcessor.js
@@ -1,45 +1,6 @@
 const ffmpeg = require("fluent-ffmpeg");
 const { PassThrough } = require("stream");
 const transcriber = require("./transcriber");
-// const { EventEmitter } = require("events");
-
-// const START_PHRASE = "I lived in Indonesia for a few years";
-// const END_PHRASE = "Monday through Friday";
-
-// let writeStream = null;
-
-// const phraseEventEmitter = new EventEmitter();
-
-// phraseEventEmitter.on("prompt_triggered", () => {
-//   writeStream = fs.createWriteStream(`./prompts/prompt-${Date.now()}.txt`, {
-//     flags: "a",
-//   });
-// });
-
-// function handleIncomingData(data) {
-//   if (writeStream) {
-//     // Write data to the file
-//     writeStream.write(data + "\n");
-
-//     writeStream.on("error", (error) => {
-//       console.error("An error occurred:", error);
-//     });
-//   }
-// }
-
-// function stopWriteStream() {
-//   if (writeStream) {
-//     writeStream.end();
-//     writeStream = null;
-//     console.log("Write stream stopped.");
-//   }
-// }
-
-// phraseEventEmitter.on("prompt_ended", async (fileName) => {
-//   console.log(fileName);
-//   stopWriteStream();
-//   return await convertTxtToString(fileName);
-// });
 
 function processAudioStream(streamPath) {
   const inputPath = `rtmp://localhost:1935${streamPath}`;
@@ -69,27 +30,6 @@ function processAudioStream(streamPath) {
   });
 }
 
-// transcriber.on("transcript", (transcript) => {
-//   if (!transcript.text) {
-//     return;
-//   }
-
-//   if (!(transcript.message_type === "PartialTranscript")) {
-//     handleIncomingData(transcript.text);
-
-//     if (transcript.text.includes(START_PHRASE)) {
-//       phraseEventEmitter.emit("prompt_triggered");
-//       console.log("Start phrase detected");
-//     }
-
-//     if (transcript.text.includes(END_PHRASE)) {
-//       phraseEventEmitter.emit("prompt_ended", writeStream.path);
-//       console.log("Stop phrase detected");
-//     }
-//   }
-// });
-
 module.exports = {
   processAudioStream,
-  // phraseEventEmitter,
 };

044d99c93cba0167cc8e8501c65d4bdb35388738
davidekete <eketedavid@gmail.com>
2024-04-01 09:42:48 +0100

    refactor: replace hardcoded paths

--- src/openai.js
+++ src/openai.js
@@ -1,13 +1,14 @@
 const { OpenAI } = require("openai");
 const fs = require("fs");
 const { textToSpeech } = require("./voiceSynthesis.js");
+const path = require("path");
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 async function getResponseFromOpenAI(fileName) {
-  const filePath = `./src/prompts/${fileName}.txt`;
+  const filePath = path.join(__dirname, "prompts", `${fileName}.txt`);
   const prompt = fs.readFileSync(filePath, { encoding: "utf-8" });
 
   const completion = await openai.chat.completions.create({
